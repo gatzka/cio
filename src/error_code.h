b3dcb7d054cb73aa4d3c29f0c38790045b6a1abf
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-03-27 17:16:57 +0200

    Take out platform specific defines for error codes.

--- src/error_code.h
+++ src/error_code.h
@@ -3,19 +3,8 @@
 
 #include "system_error_code.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifndef EWOULDBLOCK
-#define EWOULDBLOCK 9901
-#endif
-
-#ifndef EAGAIN
-#define EAGAIN 9902
-#endif
-
 enum ciol_error {
+	success = 0,
 	operation_would_block = EWOULDBLOCK,
 	resource_unavailable_try_again = EAGAIN
 };
