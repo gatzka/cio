fa8f37eaae652314490aeae11c45fa9bcec0977d
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-03-27 21:49:14 +0200

    Document error codes.

--- src/error_code.h
+++ src/error_code.h
@@ -3,10 +3,18 @@
 
 #include "system_error_code.h"
 
+/**
+ * \file
+ * @brief This file contains the declaration of the enumeration for all
+ * error codes.
+ */
+
+/**
+ * @brief All Error codes used inside the cio libary.
+ */
 enum cio_error {
-	success = 0,
-	operation_would_block = EWOULDBLOCK,
-	resource_unavailable_try_again = EAGAIN
+	cio_success = 0, /*!< No error occured. */
+	cio_no_space_left_on_device = ENOSPC, /*!< No space left on device. */
 };
 
 #ifdef __cplusplus
