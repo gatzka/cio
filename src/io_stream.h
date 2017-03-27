67ed0b055fcaa52b421e45e96ac6ea064e774d85
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-03-27 18:01:47 +0200

    Comment members of io_stream.

--- src/io_stream.h
+++ src/io_stream.h
@@ -12,9 +12,41 @@ extern "C" {
 
 typedef void (*ciol_read_handler)(void *handler_context, enum ciol_error err, uint8_t *buf, size_t len);
 
+/**
+ * @brief This structure describes the interface all implementations
+ * have to fulfill.
+ */
 struct ciol_io_stream {
+	/**
+	 * @brief The context pointer which is passed to the
+	 * ciol_io_stream::read and ciol_io_stream::close functions.
+	 */
 	void *context;
+
+	/**
+	 * @brief Read upto @p count bytes into the buffer @p buf starting
+	 * with offset @p offset.
+	 *
+	 * @param context A pointer to the ciol_io_stream::context of the
+	 * implementation implementing this interface.
+	 * @param buf The buffer to be filled.
+	 * @param offset The start offset in @p buf at which the data is
+	 * written.
+	 * @param count The maximum number of bytes to read.
+	 * @param handler The callback function to be called when the read
+	 * request is (partly) fulfilled.
+	 * @param handler_context A pointer to a context which might be
+	 * useful inside @p handler
+	 */
 	void (*read)(void *context, void *buf, off_t offset, size_t count, read_handler handler, void *handler_context);
+
+	/**
+	 * @brief Closes the stream
+	 *
+	 * Implementations implementing this interface are strongly
+	 * encouraged to flush any write buffers and to free other resources
+	 * associated with this stream.
+	 */
 	void (*close)(void *context);
 };
 
