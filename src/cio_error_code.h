d0feac9d37a84b24ce017372754f63347de55ddf
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-04-03 18:19:27 +0200

    Add some more enums for errors.

--- src/cio_error_code.h
+++ src/cio_error_code.h
@@ -15,6 +15,13 @@
 enum cio_error {
 	cio_success = 0, /*!< No error occured. */
 	cio_no_space_left_on_device = ENOSPC, /*!< No space left on device. */
+	cio_permission_denied = EACCES, /*!< Permission denied. */
+	cio_address_family_not_supported = EAFNOSUPPORT, /*!< Address family not supported. */
+	cio_invalid_argument = EINVAL, /*!< Invalid argument. */
+	cio_too_many_files_open = EMFILE, /*!< Too many files open. */
+	cio_no_buffer_space = ENOBUFS, /*!< No buffer space. */
+	cio_not_enough_memory = ENOMEM, /*!< Not enough memory. */
+	cio_protocol_not_supported = EPROTONOSUPPORT, /*!< Protocol not supported. */
 };
 
 #ifdef __cplusplus
