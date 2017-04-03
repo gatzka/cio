a7fdf3f1d81025af37cd9fd7251cf514f576362d
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-04-03 18:19:13 +0200

    Remove ifdefs from system error codes.
    Including errno.h is sufficient.

--- src/linux/cio_system_error_code.h
+++ src/linux/cio_system_error_code.h
@@ -3,22 +3,6 @@
 
 #include <errno.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifndef EWOULDBLOCK
-#define EWOULDBLOCK 9901
-#endif
-
-#ifndef EAGAIN
-#define EAGAIN 9902
-#endif
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif
 
 
