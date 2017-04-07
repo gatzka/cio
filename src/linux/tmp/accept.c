874b08abeecef1fc451f83de2e3c5a8db29612a9
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-04-07 12:08:21 +0200

    Add preliminary test.

--- src/linux/tmp/accept.c
+++ src/linux/tmp/accept.c
@@ -1,9 +1,28 @@
+#include <stddef.h>
+#include <stdio.h>
+
 #include "../cio_linux_epoll.h"
+#include "../cio_linux_server_socket.h"
 #include "../../cio_error_code.h"
+
+static void accept_handler(void *handler_context, enum cio_error err, struct cio_socket *socket)
+{
+	(void)handler_context;
+	(void)socket;
+	(void)err;
+	printf("Angekommen!\n");
+}
 
 int main()
 {
-	enum cio_error err = cio_linux_eventloop_init();
-	(void)err;
+	int go_ahead = 1;
+	cio_linux_eventloop_init();
+
+	struct cio_linux_server_socket ss_linux;
+
+	const struct cio_server_socket *ss = cio_server_socket_linux_init(&ss_linux, NULL);
+	ss->init(ss->context, 12345, 5, NULL);
+	ss->accept(ss->context, accept_handler, NULL);
+	cio_linux_eventloop_run(&go_ahead);
 	return 0;
 }
