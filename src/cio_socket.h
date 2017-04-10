778d5f206e9e3874185c2e7698014890a947665f
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-04-10 17:21:56 +0200

    Document a socket.

--- src/cio_socket.h
+++ src/cio_socket.h
@@ -7,6 +7,14 @@
 extern "C" {
 #endif
 
+/**
+ * @file
+ * @brief This file contains the interface of a socket.
+ *
+ * You can @ref cio_socket_get_input_stream "get an input stream"
+ * from a socket or @ref cio_socket_close "close" the socket.
+ */
+
 struct cio_socket {
 	/**
 	 * @brief The context pointer which is passed to the functions
@@ -14,7 +22,23 @@ struct cio_socket {
 	 */
 	void *context;
 
+	/**
+	 * @anchor cio_socket_get_input_stream
+	 * @brief Closes the cio_server_socket.
+	 *
+	 * @param context The cio_server_socket::context.
+	 */
 	struct cio_io_stream *(*get_input_stream)(void *context);
+
+	/**
+	 * @anchor cio_socket_close
+	 * @brief Closes the cio_socket.
+	 *
+	 * Once a socket has been closed, no further communication is possible. Closing the socket
+	 * also closes the socket's cio_io_stream.
+	 *
+	 * @param context The cio_server_socket::context.
+	 */
 	void (*close)(void *context);
 };
 
