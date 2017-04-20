c16d18cfe3b76bba891683b08894fc3c7c41ff2e
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-04-20 12:30:53 +0200

    Test behaviour when accept would block.

--- src/linux/tests/test_cio_linux_server_socket.c
+++ src/linux/tests/test_cio_linux_server_socket.c
@@ -171,6 +171,16 @@ static int custom_accept_fake(int fd, struct sockaddr *addr, socklen_t *addrlen)
 	}
 }
 
+static int accept_wouldblock(int fd, struct sockaddr *addr, socklen_t *addrlen)
+{
+	(void)fd;
+	(void)addr;
+	(void)addrlen;
+
+	errno = EWOULDBLOCK;
+	return -1;
+}
+
 static void accept_handler_close_server_socket(struct cio_server_socket *ss, void *handler_context, enum cio_error err, struct cio_socket *socket)
 {
 	(void)handler_context;
@@ -192,7 +202,6 @@ static void test_accept_bind_address(void) {
 
 	TEST_ASSERT_EQUAL(1, accept_handler_fake.call_count);
 	TEST_ASSERT_EQUAL(1, on_close_fake.call_count);
-	TEST_ASSERT_EQUAL(1, setsockopt_fake.call_count);
 }
 
 static void test_accept_close_in_accept_handler(void) {
@@ -208,7 +217,24 @@ static void test_accept_close_in_accept_handler(void) {
 
 	TEST_ASSERT_EQUAL(1, accept_handler_fake.call_count);
 	TEST_ASSERT_EQUAL(1, on_close_fake.call_count);
-	TEST_ASSERT_EQUAL(1, setsockopt_fake.call_count);
+}
+
+static void test_accept_wouldblock(void) {
+	accept_fake.custom_fake = accept_wouldblock;
+	accept_handler_fake.custom_fake = accept_handler_close_server_socket;
+	on_close_fake.custom_fake = close_do_nothing;
+
+	struct cio_linux_eventloop_epoll loop;
+	struct cio_linux_server_socket ss_linux;
+	const struct cio_server_socket *ss = cio_linux_server_socket_init(&ss_linux, &loop, on_close);
+	enum cio_error err = ss->init(ss->context, 12345, 5, NULL);
+	TEST_ASSERT_EQUAL(cio_success, err);
+	err = ss->accept(ss->context, accept_handler, NULL);
+	TEST_ASSERT_EQUAL(cio_success, err);
+
+	TEST_ASSERT_EQUAL(0, accept_handler_fake.call_count);
+	ss->close(ss->context);
+	TEST_ASSERT_EQUAL(1, on_close_fake.call_count);
 }
 
 static void test_accept_close_and_free_in_accept_handler(void) {
@@ -327,6 +353,7 @@ int main(void) {
 	RUN_TEST(test_accept_close_and_free_in_accept_handler);
 	RUN_TEST(test_accept_no_handler);
 	RUN_TEST(test_accept_eventloop_add_fails);
+	RUN_TEST(test_accept_wouldblock);
 	RUN_TEST(test_init_fails_no_socket);
 	RUN_TEST(test_init_listen_fails);
 	RUN_TEST(test_init_setsockopt_fails);
