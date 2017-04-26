2f2bfb652170d419befe0f1c207bb01d8dcdaa1e
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-04-26 17:50:53 +0200

    Test notifying a single event.

--- src/linux/tests/test_cio_linux_epoll.c
+++ src/linux/tests/test_cio_linux_epoll.c
@@ -38,13 +38,21 @@ FAKE_VALUE_FUNC(int, epoll_ctl, int, int, int, struct epoll_event *)
 FAKE_VALUE_FUNC(int, epoll_wait, int, struct epoll_event *, int, int)
 FAKE_VALUE_FUNC(int, close, int)
 
+FAKE_VOID_FUNC(epoll_callback, void *);
+
+static unsigned int events_in_list = 0;
+static struct cio_linux_event_notifier *ev[100];
+
 void setUp(void)
 {
 	FFF_RESET_HISTORY();
 
 	RESET_FAKE(epoll_create);
 	RESET_FAKE(epoll_ctl);
+	RESET_FAKE(epoll_wait);
 	RESET_FAKE(close);
+	RESET_FAKE(epoll_callback);
+	events_in_list = 0;
 }
 
 static int epoll_create_fail(int size)
@@ -66,6 +74,34 @@ static int epoll_ctl_fail(int epfd, int op, int fd, struct epoll_event *event)
 	errno = ENOSPC;
 
 	return -1;
+}
+
+static int epoll_ctl_save(int epfd, int op, int fd, struct epoll_event *event)
+{
+	(void)epfd;
+	(void)fd;
+
+	if (op == EPOLL_CTL_ADD) {
+		ev[events_in_list++] = event->data.ptr;
+	}
+
+	return 0;
+}
+
+static int notify_single_fd(int epfd, struct epoll_event *events,
+                            int maxevents, int timeout)
+{
+	(void)epfd;
+	(void)maxevents;
+	(void)timeout;
+
+	if (epoll_wait_fake.call_count == 1) {
+		events->events = EPOLLIN;
+		events->data.ptr = ev[0];
+		return 1;
+	} else {
+		return -1;
+	}
 }
 
 static void test_create_loop(void)
@@ -153,6 +189,36 @@ static void test_add_event_fails(void)
 	TEST_ASSERT_EQUAL(1, close_fake.call_count);
 }
 
+static void test_notify_event(void)
+{
+	epoll_wait_fake.custom_fake = notify_single_fd;
+	epoll_ctl_fake.custom_fake = epoll_ctl_save;
+
+	struct cio_linux_eventloop_epoll loop;
+	enum cio_error err = cio_linux_eventloop_init(&loop);
+	TEST_ASSERT_EQUAL(cio_success, err);
+	TEST_ASSERT_EQUAL(1, epoll_create_fake.call_count);
+
+	static const int fake_fd = 42;
+	struct cio_linux_event_notifier ev;
+	ev.fd = fake_fd;
+	ev.callback = epoll_callback;
+	ev.context = &loop;
+	err = cio_linux_eventloop_add(&loop, &ev);
+	TEST_ASSERT_EQUAL(cio_success, err);
+	TEST_ASSERT_EQUAL(1, epoll_ctl_fake.call_count);
+	TEST_ASSERT_EQUAL(loop.epoll_fd, epoll_ctl_fake.arg0_val);
+	TEST_ASSERT_EQUAL(EPOLL_CTL_ADD, epoll_ctl_fake.arg1_val);
+	TEST_ASSERT_EQUAL(fake_fd, epoll_ctl_fake.arg2_val);
+
+	err = cio_linux_eventloop_run(&loop);
+	TEST_ASSERT_EQUAL(1, epoll_callback_fake.call_count);
+	TEST_ASSERT_EQUAL(&loop, epoll_callback_fake.arg0_val);
+
+	cio_linux_eventloop_destroy(&loop);
+	TEST_ASSERT_EQUAL(1, close_fake.call_count);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
@@ -161,5 +227,6 @@ int main(void)
 	RUN_TEST(test_add_event);
 	RUN_TEST(test_add_event_fails);
 	RUN_TEST(test_cancel);
+	RUN_TEST(test_notify_event);
 	return UNITY_END();
 }
