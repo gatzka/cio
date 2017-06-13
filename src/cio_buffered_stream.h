85ecee79a6329133bdfaec04ed926776ff464a1c
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-06-13 20:33:21 +0200

    Take out the init function pointer.
    This will be done later by a free function.

--- src/cio_buffered_stream.h
+++ src/cio_buffered_stream.h
@@ -85,24 +85,9 @@ typedef void (*cio_buffered_stream_write_handler)(struct cio_buffered_stream *co
 struct cio_buffered_stream {
 
 	/**
-	 * @brief Initializes a cio_buffered_stream.
-	 *
-	 * @param context A pointer to the cio_buffered_stream::context of the
-	 * implementation implementing this interface.
-	 * @param io A pointer to an cio_io_stream which is used to fill the
-	 * internal read buffer an to which the internal write buffer will
-	 * be flushed.
-	 * @param allocator The cio_buffer_allocator which is used to allocate the
-	 * internal read and write buffers.
-	 * @return Zero for success, -1 in case of an error.
-	 */
-	int (*init)(struct cio_buffered_stream *context, const struct cio_io_stream *io, const struct cio_buffer_allocator *allocator);
-
-	/**
 	 * @brief Call @p handler if exactly @p num bytes are read.
 	 *
-	 * @param context A pointer to the cio_buffered_stream::context of the
-	 * implementation implementing this interface.
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 * @param num The number of bytes to be read when @p handler will be called.
 	 * @param handler The callback function to be called when the read
 	 * request is fulfilled.
@@ -115,7 +100,7 @@ struct cio_buffered_stream {
 	 * @brief Read upto @p count bytes into the buffer @p buf starting
 	 * with offset @p offset.
 	 *
-	 * @param context A pointer to the cio_buffered_stream the the read operates on.
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 * @param buf The buffer to be filled.
 	 * @param offset The start offset in @p buf at which the data is
 	 * written.
@@ -130,7 +115,7 @@ struct cio_buffered_stream {
 	/**
 	 * @brief Call @p handler if delimiter @p delim is encountered.
 	 *
-	 * @param context A pointer to the cio_buffered_stream the read operates on.
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 * @param delim A zero terminated string containing the delimiter to be found.
 	 * @param handler The callback function to be called when the read
 	 * request is fulfilled.
@@ -145,7 +130,7 @@ struct cio_buffered_stream {
 	 * Please note that the data written is not immediatly forwarded to
 	 * the underlying cio_io_stream. Call cio_buffered_stream::flush to do so.
 	 *
-	 * @param context A pointer to the cio_buffered_stream the write operates on.
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 * @param buf The buffer where the data is written from.
 	 * @param count The number of to write.
 	 * @param handler The callback function to be called when the write
@@ -158,6 +143,8 @@ struct cio_buffered_stream {
 	/**
 	 * Drains the data in the write buffer out to the underlying
 	 * cio_io_stream.
+	 *
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 */
 	void (*flush)(struct cio_buffered_stream *context);
 
@@ -167,6 +154,8 @@ struct cio_buffered_stream {
 	 * Implementations implementing this interface are strongly
 	 * encouraged to flush any write buffers and to free other resources
 	 * associated with this stream.
+	 *
+	 * @param context A pointer to the cio_buffered_stream of the on which the operation should be performed.
 	 */
 	void (*close)(struct cio_buffered_stream *context);
 };
