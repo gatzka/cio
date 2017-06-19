6e4f4e4fa9fbddd6a42646ff02910cae1378972e
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-06-19 13:27:20 +0200

    Test behavior of tcp_nodelay.

--- src/linux/tests/test_cio_linux_socket.c
+++ src/linux/tests/test_cio_linux_socket.c
@@ -25,6 +25,10 @@
  */
 
 #include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "fff.h"
@@ -42,14 +46,28 @@ FAKE_VALUE_FUNC(enum cio_error, cio_linux_eventloop_register_write, const struct
 FAKE_VOID_FUNC(cio_linux_eventloop_remove, struct cio_eventloop *, const struct cio_event_notifier *)
 
 FAKE_VALUE_FUNC(int, close, int)
+FAKE_VALUE_FUNC(int, setsockopt, int, int, int, const void *, socklen_t)
 
 FAKE_VALUE_FUNC0(int, cio_linux_socket_create)
 
 void on_close(struct cio_socket *s);
-FAKE_VOID_FUNC(on_close, struct cio_socket*)
+FAKE_VOID_FUNC(on_close, struct cio_socket *)
 
 static int socket_create_fails(void)
 {
+	errno = EINVAL;
+	return -1;
+}
+
+static int setsockopt_fails(int fd, int level, int option_name,
+                            const void *option_value, socklen_t option_len)
+{
+	(void)fd;
+	(void)level;
+	(void)option_name;
+	(void)option_value;
+	(void)option_len;
+
 	errno = EINVAL;
 	return -1;
 }
@@ -64,6 +82,7 @@ void setUp(void)
 	RESET_FAKE(cio_linux_eventloop_register_write);
 
 	RESET_FAKE(close);
+	RESET_FAKE(setsockopt);
 
 	RESET_FAKE(cio_linux_socket_create);
 	RESET_FAKE(on_close);
@@ -80,7 +99,7 @@ static void test_socket_init(void)
 static void test_socket_init_socket_create_fails(void)
 {
 	struct cio_socket s;
-	cio_linux_socket_create_fake.custom_fake= socket_create_fails;
+	cio_linux_socket_create_fake.custom_fake = socket_create_fails;
 
 	enum cio_error err = cio_socket_init(&s, NULL, NULL);
 	TEST_ASSERT(err != cio_success);
@@ -121,6 +140,53 @@ static void test_socket_close_with_hook(void)
 	TEST_ASSERT_EQUAL(&s, on_close_fake.arg0_val);
 }
 
+static void test_socket_enable_nodelay(void)
+{
+	struct cio_socket s;
+
+	enum cio_error err = cio_socket_init(&s, NULL, on_close);
+	TEST_ASSERT_EQUAL(cio_success, err);
+
+	err = s.set_tcp_no_delay(&s, true);
+	TEST_ASSERT_EQUAL(cio_success, err);
+	TEST_ASSERT_EQUAL(1, setsockopt_fake.call_count);
+	TEST_ASSERT_EQUAL(s.ev.fd, setsockopt_fake.arg0_val);
+	TEST_ASSERT_EQUAL(IPPROTO_TCP, setsockopt_fake.arg1_val);
+	TEST_ASSERT_EQUAL(TCP_NODELAY, setsockopt_fake.arg2_val);
+}
+
+static void test_socket_disable_nodelay(void)
+{
+	struct cio_socket s;
+
+	enum cio_error err = cio_socket_init(&s, NULL, on_close);
+	TEST_ASSERT_EQUAL(cio_success, err);
+
+	err = s.set_tcp_no_delay(&s, false);
+	TEST_ASSERT_EQUAL(cio_success, err);
+	TEST_ASSERT_EQUAL(1, setsockopt_fake.call_count);
+	TEST_ASSERT_EQUAL(s.ev.fd, setsockopt_fake.arg0_val);
+	TEST_ASSERT_EQUAL(IPPROTO_TCP, setsockopt_fake.arg1_val);
+	TEST_ASSERT_EQUAL(TCP_NODELAY, setsockopt_fake.arg2_val);
+}
+
+static void test_socket_nodelay_setsockopt_fails(void)
+{
+	struct cio_socket s;
+
+	setsockopt_fake.custom_fake = setsockopt_fails;
+
+	enum cio_error err = cio_socket_init(&s, NULL, on_close);
+	TEST_ASSERT_EQUAL(cio_success, err);
+
+	err = s.set_tcp_no_delay(&s, false);
+	TEST_ASSERT(cio_success != err);
+	TEST_ASSERT_EQUAL(1, setsockopt_fake.call_count);
+	TEST_ASSERT_EQUAL(s.ev.fd, setsockopt_fake.arg0_val);
+	TEST_ASSERT_EQUAL(IPPROTO_TCP, setsockopt_fake.arg1_val);
+	TEST_ASSERT_EQUAL(TCP_NODELAY, setsockopt_fake.arg2_val);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
@@ -129,5 +195,8 @@ int main(void)
 	RUN_TEST(test_socket_init_eventloop_add_fails);
 	RUN_TEST(test_socket_close_without_hook);
 	RUN_TEST(test_socket_close_with_hook);
+	RUN_TEST(test_socket_enable_nodelay);
+	RUN_TEST(test_socket_disable_nodelay);
+	RUN_TEST(test_socket_nodelay_setsockopt_fails);
 	return UNITY_END();
 }
