08a777d7a6316d659be77b483b58e87696edec94
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-06-29 17:26:59 +0200

    Implement a system allocator for Linux.

--- src/linux/cio_linux_alloc.c
+++ src/linux/cio_linux_alloc.c
@@ -26,6 +26,7 @@
 
 #include <stdlib.h>
 
+#include "cio_buffer_allocator.h"
 #include "cio_linux_alloc.h"
 
 void *cio_malloc(size_t size)
@@ -36,4 +37,30 @@ void *cio_malloc(size_t size)
 void cio_free(void *ptr)
 {
 	free(ptr);
+}
+
+static struct cio_buffer allocate(struct cio_buffer_allocator *context, size_t size)
+{
+	(void)context;
+	struct cio_buffer buffer;
+	buffer.address = malloc(size);
+	buffer.size = size;
+	return buffer;
+}
+
+static void free_mem(struct cio_buffer_allocator *context, void *ptr)
+{
+	(void)context;
+	free(ptr);
+}
+
+static struct cio_buffer_allocator linux_system_allocator = {
+	.alloc = allocate,
+	.free = free_mem
+};
+
+
+struct cio_buffer_allocator *get_system_allocator(void)
+{
+	return &linux_system_allocator;
 }
