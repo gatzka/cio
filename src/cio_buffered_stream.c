bae457cdd5bb8e1452975b7832a271b8ad7f4cd5
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-07-28 15:58:14 +0200

    Initialize a buffered stream.

--- src/cio_buffered_stream.c
+++ src/cio_buffered_stream.c
@@ -25,9 +25,53 @@
  */
 
 #include <cio_allocator.h>
+#include <cio_compiler.h>
 #include <cio_buffered_stream.h>
 #include <cio_error_code.h>
 #include <cio_io_stream.h>
+
+static void bs_read(struct cio_buffered_stream *context, size_t num, cio_buffered_stream_read_handler handler, void *handler_context)
+{
+	(void)context;
+	(void)num;
+	(void)handler;
+	(void)handler_context;
+}
+
+static void bs_read_until(struct cio_buffered_stream *context, const char *delim, cio_buffered_stream_read_handler handler, void *handler_context)
+{
+	(void)context;
+	(void)delim;
+	(void)handler;
+	(void)handler_context;
+}
+
+static void bs_read_exactly(struct cio_buffered_stream *context, size_t num, cio_buffered_stream_read_handler handler, void *handler_context)
+{
+	(void)context;
+	(void)num;
+	(void)handler;
+	(void)handler_context;
+}
+
+static void bs_write(struct cio_buffered_stream *context, const void *buf, size_t count, cio_buffered_stream_write_handler handler, void *handler_context)
+{
+	(void)context;
+	(void)buf;
+	(void)count;
+	(void)handler;
+	(void)handler_context;
+}
+
+static void bs_flush(struct cio_buffered_stream *context)
+{
+	(void)context;
+}
+
+static void bs_close(struct cio_buffered_stream *context)
+{
+	(void)context;
+}
 
 enum cio_error cio_buffered_stream_init(struct cio_buffered_stream *bs,
                                         struct cio_io_stream *stream,
@@ -36,14 +80,35 @@ enum cio_error cio_buffered_stream_init(struct cio_buffered_stream *bs,
                                         size_t write_buffer_size,
                                         struct cio_allocator *write_buffer_allocator)
 {
-	if ((read_buffer_allocator == NULL) || (write_buffer_allocator == NULL)) {
+	if (unlikely((read_buffer_allocator == NULL) || (write_buffer_allocator == NULL))) {
 		return cio_invalid_argument;
 	}
 
 	bs->stream = stream;
-	bs->read_buffer_size = read_buffer_size;
+	struct cio_buffer read_buffer = read_buffer_allocator->alloc(read_buffer_allocator, read_buffer_size);
+	if (unlikely(read_buffer.address == NULL)) {
+		return cio_not_enough_memory;
+	}
+
+	bs->read_buffer = read_buffer.address;
+	bs->read_buffer_size = read_buffer.size;
 	bs->read_buffer_allocator = read_buffer_allocator;
-	bs->write_buffer_size = write_buffer_size;
+
+	struct cio_buffer write_buffer = write_buffer_allocator->alloc(write_buffer_allocator, write_buffer_size);
+	if (unlikely(write_buffer.address == NULL)) {
+		read_buffer_allocator->free(read_buffer_allocator, read_buffer.address);
+		return cio_not_enough_memory;
+	}
+
+	bs->write_buffer = write_buffer.address;
+	bs->write_buffer_size = write_buffer.size;
 	bs->write_buffer_allocator = write_buffer_allocator;
+
+	bs->read = bs_read;
+	bs->read_exactly = bs_read_exactly;
+	bs->read_until = bs_read_until;
+	bs->write = bs_write;
+	bs->flush = bs_flush;
+	bs->close = bs_close;
 	return cio_success;
 }
