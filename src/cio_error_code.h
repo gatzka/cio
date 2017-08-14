dafb52be2f893170f936e4a9125ded01c5b16fc7
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-08-14 13:36:57 +0200

    Correct formatting.

--- src/cio_error_code.h
+++ src/cio_error_code.h
@@ -39,31 +39,31 @@
  * @brief All error codes used inside the cio libary.
  */
 enum cio_error {
-	cio_success = 0,                                 /*!< No error occured. */
+	cio_success = 0, /*!< No error occured. */
 	cio_address_family_not_supported = EAFNOSUPPORT, /*!< Address family not supported. */
-	cio_address_in_use = EADDRINUSE,                 /*!< Address is already in use. */
-	cio_address_not_available = EADDRNOTAVAIL,       /*!< Address not available */
-	cio_bad_address = EFAULT,                        /*!< Bad address. */
-	cio_bad_file_descriptor = EBADF,                 /*!< Bad file descriptor. */
-	cio_file_exists = EEXIST,                        /*!< File exists. */
-	cio_filename_too_long = ENAMETOOLONG,            /*!< File name too long. */
-	cio_invalid_argument = EINVAL,                   /*!< Invalid argument. */
-	cio_no_buffer_space = ENOBUFS,                   /*!< No buffer space. */
-	cio_no_protocol_option = ENOPROTOOPT,            /*!< No protocol option. */
-	cio_no_space_left_on_device = ENOSPC,            /*!< No space left on device. */
-	cio_no_such_file_or_directory = ENOENT,          /*!< No such file or directory. */
-	cio_not_a_directory = ENOTDIR,                   /*!< Not a directory. */
-	cio_not_a_socket = ENOTSOCK,                     /*!< Not a socket. */
-	cio_not_enough_memory = ENOMEM,                  /*!< Not enough memory. */
-	cio_operation_not_permitted = EPERM,             /*!< Operation not permitted. */
-	cio_permission_denied = EACCES,                  /*!< Permission denied. */
-	cio_protocol_not_supported = EPROTONOSUPPORT,    /*!< Protocol not supported. */
-	cio_read_only_file_system = EROFS,               /*!< Read only file system. */
-	cio_too_many_files_open = EMFILE,                /*!< Too many files open. */
-	cio_too_many_symbolic_link_levels = ELOOP,       /*!< Too many symbolic link levels. */
-	cio_operation_aborted = ECANCELED,               /*!< Operation cancelled. */
-	cio_no_such_device = ENODEV,                     /*!< No such device. */
-	cio_message_too_long = EMSGSIZE                  /*!< Message too long. */
+	cio_address_in_use = EADDRINUSE, /*!< Address is already in use. */
+	cio_address_not_available = EADDRNOTAVAIL, /*!< Address not available */
+	cio_bad_address = EFAULT, /*!< Bad address. */
+	cio_bad_file_descriptor = EBADF, /*!< Bad file descriptor. */
+	cio_file_exists = EEXIST, /*!< File exists. */
+	cio_filename_too_long = ENAMETOOLONG, /*!< File name too long. */
+	cio_invalid_argument = EINVAL, /*!< Invalid argument. */
+	cio_no_buffer_space = ENOBUFS, /*!< No buffer space. */
+	cio_no_protocol_option = ENOPROTOOPT, /*!< No protocol option. */
+	cio_no_space_left_on_device = ENOSPC, /*!< No space left on device. */
+	cio_no_such_file_or_directory = ENOENT, /*!< No such file or directory. */
+	cio_not_a_directory = ENOTDIR, /*!< Not a directory. */
+	cio_not_a_socket = ENOTSOCK, /*!< Not a socket. */
+	cio_not_enough_memory = ENOMEM, /*!< Not enough memory. */
+	cio_operation_not_permitted = EPERM, /*!< Operation not permitted. */
+	cio_permission_denied = EACCES, /*!< Permission denied. */
+	cio_protocol_not_supported = EPROTONOSUPPORT, /*!< Protocol not supported. */
+	cio_read_only_file_system = EROFS, /*!< Read only file system. */
+	cio_too_many_files_open = EMFILE, /*!< Too many files open. */
+	cio_too_many_symbolic_link_levels = ELOOP, /*!< Too many symbolic link levels. */
+	cio_operation_aborted = ECANCELED, /*!< Operation cancelled. */
+	cio_no_such_device = ENODEV, /*!< No such device. */
+	cio_message_too_long = EMSGSIZE /*!< Message too long. */
 };
 
 #ifdef __cplusplus
