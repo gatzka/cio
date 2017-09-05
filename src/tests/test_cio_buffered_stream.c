42e466e98182fa3c727d40a270c14c01cea154f4
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-09-05 08:46:48 +0200

    Test if read_exactly works for the normal case.

--- src/tests/test_cio_buffered_stream.c
+++ src/tests/test_cio_buffered_stream.c
@@ -36,16 +36,35 @@
 
 DEFINE_FFF_GLOBALS
 
+#undef container_of
+#define container_of(ptr, type, member) ( \
+	(void *)((char *)ptr - offsetof(type, member)))
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
 struct memory_stream {
 	struct cio_io_stream ios;
 	void *mem;
 };
+
+static uint8_t check_buffer[100];
 
 void read_some(struct cio_io_stream *ios, void *buf, size_t num, cio_io_stream_read_handler handler, void *context);
 FAKE_VOID_FUNC(read_some, struct cio_io_stream*, void*, size_t, cio_io_stream_read_handler, void*)
 
 void close(struct cio_io_stream *context);
 FAKE_VOID_FUNC(close, struct cio_io_stream*)
+
+void dummy_handler(struct cio_buffered_stream*, void*, enum cio_error, uint8_t*, size_t);
+FAKE_VOID_FUNC(dummy_handler, struct cio_buffered_stream*, void*, enum cio_error, uint8_t*, size_t)
+
+static void save_to_check_buffer(struct cio_buffered_stream *bs, void *context, enum cio_error err, uint8_t *buffer, size_t num) {
+	(void)bs;
+	(void)context;
+	if (err == cio_success) {
+		memcpy(check_buffer, buffer, num);
+	}
+}
 
 static struct cio_buffer alloc_no_mem(struct cio_allocator *context, size_t size)
 {
@@ -55,6 +74,14 @@ static struct cio_buffer alloc_no_mem(struct cio_allocator *context, size_t size
 	buffer.address = NULL;
 	buffer.size = 0;
 	return buffer;
+}
+
+static void read_some_max(struct cio_io_stream *ios, void *buf, size_t num, cio_io_stream_read_handler handler, void *context)
+{
+	struct memory_stream *ms = container_of(ios, struct memory_stream, ios);
+	size_t len = MIN(num, strlen(ms->mem) + 1);
+	memcpy(buf, ms->mem, len);
+	handler(ios, context, cio_success, buf, len);
 }
 
 static void memory_stream_init(struct memory_stream *ms, const char *fill_pattern)
@@ -84,6 +111,7 @@ void setUp(void)
 
 	RESET_FAKE(read_some);
 	RESET_FAKE(close);
+	RESET_FAKE(dummy_handler);
 }
 
 static void test_init_missing_stream(void)
@@ -136,6 +164,27 @@ static void test_init_alloc_write_fails(void)
 	TEST_ASSERT_EQUAL(cio_not_enough_memory, err);
 }
 
+static void test_read_exactly(void)
+{
+	static const char *test_data = "Hello";
+	memory_stream_init(&ms, test_data);
+	read_some_fake.custom_fake = read_some_max;
+	dummy_handler_fake.custom_fake = save_to_check_buffer;
+
+	struct cio_buffered_stream bs;
+	enum cio_error err = cio_buffered_stream_init(&bs, &ms.ios, 40, cio_get_system_allocator(), 30, cio_get_system_allocator());
+	TEST_ASSERT_EQUAL(cio_success, err);
+	bs.read_exactly(&bs, strlen(test_data), dummy_handler, check_buffer);
+
+	bs.close(&bs);
+	TEST_ASSERT_EQUAL_MESSAGE(1, close_fake.call_count, "Underlying cio_iostream was not closed!");
+	TEST_ASSERT_EQUAL_MESSAGE(1, dummy_handler_fake.call_count, "Handler was not called!");
+	TEST_ASSERT_EQUAL_MESSAGE(cio_success, dummy_handler_fake.arg2_val, "Handler was not called with cio_success!");
+	TEST_ASSERT_EQUAL_MESSAGE(strlen(test_data), dummy_handler_fake.arg4_val, "Handler was not called with correct data length!");
+	TEST_ASSERT_MESSAGE(strcmp((const char *)check_buffer, test_data) == 0, "Handler was not called with correct data!")
+	memory_stream_deinit(&ms);
+}
+
 int main(void)
 {
 	UNITY_BEGIN();
@@ -145,5 +194,6 @@ int main(void)
 	RUN_TEST(test_init_correctly);
 	RUN_TEST(test_init_alloc_read_fails);
 	RUN_TEST(test_init_alloc_write_fails);
+	RUN_TEST(test_read_exactly);
 	return UNITY_END();
 }
