0b310ce5b00dddce89fb52a29f8af090dac92603
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-10-15 17:50:51 +0200

    Feature/init request target (#121)
    * Take out unnecessary checks.
    
    * Test initialization of request target structure.

--- src/tests/test_cio_http_server.c
+++ src/tests/test_cio_http_server.c
@@ -33,33 +33,66 @@
 #include "cio_error_code.h"
 #include "cio_http_server.h"
 #include "cio_server_socket.h"
+#include "cio_write_buffer.h"
 
 #undef container_of
 #define container_of(ptr, type, member) ( \
 	(void *)((char *)ptr - offsetof(type, member)))
 
+struct dummy_handler {
+	struct cio_http_request_handler handler;
+	struct cio_write_buffer wbh;
+	struct cio_write_buffer wb;
+};
+
+static const size_t read_buffer_size = 200;
+
 DEFINE_FFF_GLOBALS
 
 FAKE_VALUE_FUNC(enum cio_error, cio_server_socket_init, struct cio_server_socket *, struct cio_eventloop *, unsigned int, cio_alloc_client, cio_free_client, cio_server_socket_close_hook)
 
-struct dummy_client {
-	struct cio_socket socket;
-};
-
 static struct cio_socket *alloc_dummy_client(void)
 {
-	struct dummy_client *dc = malloc(sizeof(*dc));
-	memset(dc, 0xaf, sizeof(*dc));
-	return &dc->socket;
+	struct cio_http_client *client = malloc(sizeof(*client) + read_buffer_size);
+	memset(client, 0xaf, sizeof(*client));
+	return &client->socket;
 }
 
 static void free_dummy_client(struct cio_socket *socket)
 {
-	struct dummy_client *dc = container_of(socket, struct dummy_client, socket);
-	free(dc);
+	struct cio_http_client *client = container_of(socket, struct cio_http_client, socket);
+	free(client);
+}
+
+static void free_dummy_handler(struct cio_http_request_handler *handler)
+{
+	struct dummy_handler *dh = container_of(handler, struct dummy_handler, handler);
+	free(dh);
+}
+
+static struct cio_http_request_handler *alloc_dummy_handler(const void *config)
+{
+	(void)config;
+	struct dummy_handler *handler = malloc(sizeof(*handler));
+	if (unlikely(handler == NULL)) {
+		return NULL;
+	} else {
+		cio_write_buffer_head_init(&handler->wbh);
+		handler->handler.free = free_dummy_handler;
+		handler->handler.on_header_field = NULL;
+		handler->handler.on_header_value = NULL;
+		handler->handler.on_url = NULL;
+		handler->handler.on_headers_complete = NULL;
+		return &handler->handler;
+	}
 }
 
 static struct cio_eventloop loop;
+
+void setUp(void)
+{
+	FFF_RESET_HISTORY();
+}
 
 static void test_server_init_correctly(void)
 {
@@ -95,9 +128,31 @@ static void test_server_init_no_free(void)
 	TEST_ASSERT_EQUAL_MESSAGE(cio_invalid_argument, err, "Initialization did not fail!");
 }
 
-void setUp(void)
+static void test_request_target_init_correctly(void)
 {
-	FFF_RESET_HISTORY();
+	struct cio_http_request_target target;
+	enum cio_error err = cio_http_request_target_init(&target, "/foo", NULL, alloc_dummy_handler);
+	TEST_ASSERT_EQUAL_MESSAGE(cio_success, err, "Initialization failed!");
+}
+
+static void test_request_target_init_no_target(void)
+{
+	enum cio_error err = cio_http_request_target_init(NULL, "/foo", NULL, alloc_dummy_handler);
+	TEST_ASSERT_EQUAL_MESSAGE(cio_invalid_argument, err, "Initialization did not fail!");
+}
+
+static void test_request_target_init_no_request_target(void)
+{
+	struct cio_http_request_target target;
+	enum cio_error err = cio_http_request_target_init(&target, NULL, NULL, alloc_dummy_handler);
+	TEST_ASSERT_EQUAL_MESSAGE(cio_invalid_argument, err, "Initialization did not fail!");
+}
+
+static void test_request_target_init_no_alloc_handler(void)
+{
+	struct cio_http_request_target target;
+	enum cio_error err = cio_http_request_target_init(&target, "/foo", NULL, NULL);
+	TEST_ASSERT_EQUAL_MESSAGE(cio_invalid_argument, err, "Initialization did not fail!");
 }
 
 int main(void)
@@ -108,5 +163,9 @@ int main(void)
 	RUN_TEST(test_server_init_no_loop);
 	RUN_TEST(test_server_init_no_alloc);
 	RUN_TEST(test_server_init_no_free);
+	RUN_TEST(test_request_target_init_correctly);
+	RUN_TEST(test_request_target_init_no_alloc_handler);
+	RUN_TEST(test_request_target_init_no_request_target);
+	RUN_TEST(test_request_target_init_no_target);
 	return UNITY_END();
 }
