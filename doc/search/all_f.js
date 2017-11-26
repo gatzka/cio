aac5211b11629f09012c5ec4d66e17cd9ea3b5d9
travis-ci <travis@travis-ci.org>
2017-11-26 23:03:11 +0000

    Latest doxygen on successful travis build 518 auto-pushed to gh-pages

--- doc/search/all_f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['timer_5fhandler',['timer_handler',['../cio__timer_8h.html#a5f39123edfcb6b2b61b55a4444b26978',1,'cio_timer.h']]]
-];
--- doc/search/variables_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['next',['next',['../structcio__write__buffer.html#a0da61c65e3e147fe1aa914b6ef701af0',1,'cio_write_buffer']]]
-];
