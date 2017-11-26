caf9ce1ed14adc7b939e47685d754163f0ad8b2b
travis-ci <travis@travis-ci.org>
2017-11-26 22:58:53 +0000

    Latest doxygen on successful travis build 517 auto-pushed to gh-pages

---
+++ doc/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['accept',['accept',['../structcio__server__socket.html#a8d268075a4383c1bfe8530d5eebdfa8f',1,'cio_server_socket']]]
+];
---
+++ doc/search/all_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['handler',['handler',['../structcio__http__client.html#a90844e5f9e1490cb15eb051c9ed11715',1,'cio_http_client']]],
+  ['http_5fmajor',['http_major',['../structcio__http__client.html#a44731b52ebf1a6e23d4be67549b36c0f',1,'cio_http_client']]],
+  ['http_5fmethod',['http_method',['../structcio__http__client.html#a87bb7cbe9882e05b9c2eaace76698246',1,'cio_http_client']]],
+  ['http_5fminor',['http_minor',['../structcio__http__client.html#aaebd126fae4cd5aeaf9b94e65e11548c',1,'cio_http_client']]]
+];
