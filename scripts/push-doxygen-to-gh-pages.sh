f093a5903da3395363f4591a8fae7482966eee07
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-11-26 23:47:35 +0100

    Some more test output.

--- scripts/push-doxygen-to-gh-pages.sh
+++ scripts/push-doxygen-to-gh-pages.sh
@@ -11,14 +11,16 @@ if [ "$TRAVIS_REPO_SLUG" == "gatzka/cio" ] && [ "$TRAVIS_PULL_REQUEST" == "false
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gatzka/cio gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gatzka/cio gh-pages
+  #git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gatzka/cio gh-pages > /dev/null
 
   cd gh-pages
   git rm -rf ./doc
   cp -Rf $HOME/doxygen-latest ./doc
   git add -f .
   git commit -m "Latest doxygen on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages
+  #git push -fq origin gh-pages > /dev/null
 
   echo -e "Published doxygen to gh-pages.\n"
   
