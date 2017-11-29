e503544fb11e9a16a681bb2c55c1e8abddfe3bc0
Stephan Gatzka <stephan.gatzka@gmail.com>
2017-11-29 08:22:15 +0100

    Document container_of.

--- src/cio_util.h
+++ src/cio_util.h
@@ -29,9 +29,21 @@
 
 #include <stddef.h>
 
+/**
+ * Utility functions.
+ */
+
+/**
+ * @hideinitializer
+ * Get the pointer to a structure encapsulating a structure to which @p ptr points to.
+ */
 #define container_of(ptr, type, member) ( \
     (void *)((char *)(ptr)-offsetof(type, member)))
 
+/**
+ * @hideinitializer
+ * Get a @p const pointer to a structure encapsulating a structure to which @p ptr points to.
+ */
 #define const_container_of(ptr, type, member) ( \
     (const void *)((const char *)(ptr)-offsetof(type, member)))
 
