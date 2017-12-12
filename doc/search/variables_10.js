c72d074a30bad7023dab4fb1e237c591c9fbe243
travis-ci <travis@travis-ci.org>
2017-12-12 15:38:12 +0000

    Latest doxygen on successful travis build 594 auto-pushed to gh-pages

--- doc/search/variables_10.js
+++ doc/search/variables_10.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['wbh',['wbh',['../structcio__http__client.html#a5c18d4350ac8432cc939e5e3cdc6fcfd',1,'cio_http_client']]],
-  ['write',['write',['../structcio__buffered__stream.html#a6659275439b9c9508e6ad10eced2ff62',1,'cio_buffered_stream']]],
-  ['write_5fbinary_5fframe',['write_binary_frame',['../structcio__websocket.html#aa1ec1dcfb77208e26ca5449eb4747232',1,'cio_websocket']]],
+  ['wbh',['wbh',['../structcio__http__client.html#a51e3f2198b4b1dda7aa58ac63270b5af',1,'cio_http_client']]],
+  ['write',['write',['../structcio__buffered__stream.html#ac8d6fdba3c6ac6d3ef2da74d6b36cfe5',1,'cio_buffered_stream']]],
+  ['write_5fbinary_5fframe',['write_binary_frame',['../structcio__websocket.html#a4445db7e9452217afed20de5aa10a926',1,'cio_websocket']]],
   ['write_5fheader',['write_header',['../structcio__http__client.html#adc1a5a87e3e01609dc4feaba730d860b',1,'cio_http_client']]],
-  ['write_5fresponse',['write_response',['../structcio__http__client.html#a18162385dfa12942296ec53777cc9db1',1,'cio_http_client']]],
-  ['write_5fsome',['write_some',['../structcio__io__stream.html#a10b56600fd43cb7f5f1a16c5e37c1c9a',1,'cio_io_stream']]],
-  ['write_5ftext_5fframe',['write_text_frame',['../structcio__websocket.html#a26a22c890fa7fa4f06564a9fa1ec792d',1,'cio_websocket']]]
+  ['write_5fresponse',['write_response',['../structcio__http__client.html#ae1733d96243494f2c79c07e139d88e7b',1,'cio_http_client']]],
+  ['write_5fsome',['write_some',['../structcio__io__stream.html#a4f37e6797ebc74baa0db6a969884d5d6',1,'cio_io_stream']]],
+  ['write_5ftext_5fframe',['write_text_frame',['../structcio__websocket.html#a0118d62d5893ab07776d7147b6f87d61',1,'cio_websocket']]]
 ];
--- doc/search/variables_4.js
+++ doc/search/variables_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['element',['element',['../structcio__write__buffer.html#ad6c795b6fb46d07c487bd9ba9b99d05b',1,'cio_write_buffer']]],
+  ['element',['element',['../structcio__const__write__buffer.html#aec21617d3ffce4162b04265add10cf68',1,'cio_const_write_buffer']]],
   ['expires_5ffrom_5fnow',['expires_from_now',['../structcio__timer.html#ab7de1507254b6b4fe5216615c0cbb333',1,'cio_timer']]]
 ];
