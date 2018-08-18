f1a7c47a7b525d01beef2f99d711237bb67d2b98
Stephan Gatzka <stephan.gatzka@gmail.com>
2018-08-18 13:17:05 +0200

    Get random numbers by BCryptGenRandom.

---
+++ src/windows/cio_windows_random.c
@@ -0,0 +1,37 @@
+/*
+ *The MIT License (MIT)
+ *
+ * Copyright (c) <2018> <Stephan Gatzka>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <windows.h>
+#include <bcrypt.h>
+#include <stddef.h>
+
+#include "cio_random.h"
+
+
+void cio_random_get_bytes(void *bytes, size_t num_bytes)
+{
+	BCryptGenRandom(NULL, bytes, num_bytes, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+}
