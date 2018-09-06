21347594ff0db9656ddb967b58a1395130b12a8d
Stephan Gatzka <stephan.gatzka@gmail.com>
2018-09-06 12:26:32 +0200

    Make remove patterns configurable.

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -30,7 +30,7 @@ if(NOT (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "Coverage
   MESSAGE( WARNING "Code coverage results with an optimized (non-Debug) build may be misleading" )
 endif()
 
-function(SETUP_TARGET_FOR_COVERAGE _targetname _outputname)
+function(SETUP_TARGET_FOR_COVERAGE _targetname _outputname _removepatterns)
 
 	if(NOT LCOV_PATH)
 		message(FATAL_ERROR "lcov not found! Aborting...")
@@ -38,10 +38,11 @@ function(SETUP_TARGET_FOR_COVERAGE _targetname _outputname)
 
 	if(NOT GENHTML_PATH)
 		message(FATAL_ERROR "genhtml not found! Aborting...")
-	endif() # NOT GENHTML_PATH
+	endif()
 
 	set(coverage_info "${CMAKE_BINARY_DIR}/${_outputname}.info")
 	set(coverage_cleaned "${coverage_info}.cleaned")
+	separate_arguments(_removepatterns)
 
 	add_custom_target(${_targetname}
 
@@ -50,17 +51,17 @@ function(SETUP_TARGET_FOR_COVERAGE _targetname _outputname)
 		COMMAND make test
 
 		COMMAND ${LCOV_PATH} --directory ${CMAKE_BINARY_DIR} --capture --quiet --rc lcov_branch_coverage=1 --output-file ${coverage_info}
-		COMMAND ${LCOV_PATH} --remove ${coverage_info} '*/tests/*' '*/http-parser/*' '*/sha1/*' '*/unity/*' '/usr/include/*' --output-file ${coverage_cleaned} --rc lcov_branch_coverage=1 --quiet
+		COMMAND ${LCOV_PATH} --remove ${coverage_info} ${_removepatterns} --output-file ${coverage_cleaned} --rc lcov_branch_coverage=1 --quiet
+		COMMAND ${GENHTML_PATH} -o ${_outputname} ${coverage_cleaned} --branch-coverage
 		COMMAND ${GENHTML_PATH} -o ${_outputname} ${coverage_cleaned} --branch-coverage --quiet
-		COMMAND ${CMAKE_COMMAND} -E remove ${coverage_info}
 
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMENT "Resetting code coverage counters to zero."
+		COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
 	)
 
 	add_custom_command(TARGET ${_targetname} POST_BUILD
 		COMMAND ;
-		COMMENT "Processing code coverage counters and generating report.\nOpen ${CMAKE_BINARY_DIR}/${_outputname}/index.html in your browser to view the coverage report."
+		COMMENT "Open ${CMAKE_BINARY_DIR}/${_outputname}/index.html in your browser to view the coverage report."
 	)
 
 endfunction()
