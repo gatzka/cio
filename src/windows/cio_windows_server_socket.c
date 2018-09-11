2c4801588ca198d89e9df0ab522c013d74e542b5
Stephan Gatzka <stephan.gatzka@gmail.com>
2018-09-11 11:12:49 +0200

    Implement reuse_address function.

--- src/windows/cio_windows_server_socket.c
+++ src/windows/cio_windows_server_socket.c
@@ -31,8 +31,26 @@
 #include "cio_server_socket.h"
 
 // TODO: Caution. When closing a socket and there are still pending overlapped operations,
-// we cannot just free the memory which holds the overlapped structure. We need to remove it from the 
+// we cannot just free the memory which holds the overlapped structure. We need to remove it from the
 // The eventloop first and only close the socket afterwards.
+
+static enum cio_error socket_set_reuse_address(struct cio_server_socket *ss, bool on)
+{
+	char reuse;
+	if (on) {
+		reuse = 1;
+	} else {
+		reuse = 0;
+	}
+
+	if (cio_unlikely(setsockopt(ss->ev.s, SOL_SOCKET, SO_REUSEADDR, &reuse,
+	                            sizeof(reuse)) < 0)) {
+		int err = GetLastError();
+		return (enum cio_error)(-err);
+	}
+
+	return CIO_SUCCESS;
+}
 
 enum cio_error cio_server_socket_init(struct cio_server_socket *ss,
                                       struct cio_eventloop *loop,
@@ -41,19 +59,40 @@ enum cio_error cio_server_socket_init(struct cio_server_socket *ss,
                                       cio_free_client free_client,
                                       cio_server_socket_close_hook close_hook)
 {
-	
-	SOCKET s_ipv4 = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	if (cio_unlikely(s_ipv4 == INVALID_SOCKET)) {
-		int err = WSAGetLastError();
-		return -err;
+	int err;
+	SOCKET s = WSASocket(AF_INET6, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (cio_unlikely(s == INVALID_SOCKET)) {
+		err = WSAGetLastError();
+		return (enum cio_error)-err;
 	}
 
-	SOCKET s_ipv6 = WSASocket(AF_INET6, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-	if (cio_unlikely(s_ipv4 == INVALID_SOCKET)) {
-		int err = WSAGetLastError();
-		closesocket(s_ipv4);
-		return -err;
+	ss->ev.s = s;
+	ss->ev.overlapped.hEvent = WSACreateEvent();
+	if (cio_unlikely(ss->ev.overlapped.hEvent == WSA_INVALID_EVENT)) {
+		err = WSAGetLastError();
+		goto create_event_failed;
 	}
+
+	u_long on = 1;
+	int ret = ioctlsocket(s, FIONBIO, &on);
+	if (cio_unlikely(ret != 0)) {
+		err = WSAGetLastError();
+		goto ioctl_failed;
+	}
+
+	ss->loop = loop;
+	ss->backlog = (int)backlog;
+	ss->alloc_client = alloc_client;
+	ss->free_client = free_client;
+	ss->close_hook = close_hook;
+
+	ss->set_reuse_address = socket_set_reuse_address;
 
 	return CIO_SUCCESS;
+
+ioctl_failed:
+	WSACloseEvent(ss->ev.overlapped.hEvent);
+create_event_failed:
+	closesocket(s);
+	return (enum cio_error)-err;
 }
