70e0a861ca58fd5054ca2402da0006f326044324
Stephan Gatzka <stephan.gatzka@hbm.com>
2018-11-06 10:50:52 +0100

    Filter output of lcov.

--- tests/collect_coverage.cmake
+++ tests/collect_coverage.cmake
@@ -1,10 +1,14 @@
-message("Collecting coverage info")
 execute_process(
     COMMAND ${LCOV_PATH} --directory ${BINARY_DIR} --capture --quiet --rc lcov_branch_coverage=1 --output-file coverage.info 
     WORKING_DIRECTORY ${BINARY_DIR}
 )
 
 execute_process(
-    COMMAND ${GENHTML_PATH} -o ${BINARY_DIR}/coverage coverage.info --branch-coverage --quiet
+    COMMAND ${LCOV_PATH} --remove coverage.info */tests/* */http-parser/* */miniz/* */sha1/* */unity/* /usr/include/* --output-file coverage.cleaned.info --rc lcov_branch_coverage=1
+    WORKING_DIRECTORY ${BINARY_DIR}
+)
+
+execute_process(
+    COMMAND ${GENHTML_PATH} -o ${BINARY_DIR}/coverage coverage.cleaned.info --branch-coverage --quiet
     WORKING_DIRECTORY ${BINARY_DIR}
 )
--- tests/reset_coverage.cmake
+++ tests/reset_coverage.cmake
@@ -3,11 +3,6 @@ if (gcda_files)
     file(REMOVE ${gcda_files})
 endif()
 
-file(GLOB_RECURSE gcno_files "${BINARY_DIR}/*.gcno")
-if (gcno_files)
-    file(REMOVE ${gcno_files})
-endif()
-
 execute_process(
     COMMAND ${LCOV_PATH} --directory ${BINARY_DIR} --zerocounters --quiet --rc lcov_branch_coverage=1
     WORKING_DIRECTORY ${BINARY_DIR}
