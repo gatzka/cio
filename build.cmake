67bf1bc2ca8c5f7bc5a05d4107e70047d11da1eb
Stephan Gatzka <stephan.gatzka@hbm.com>
2018-11-21 14:08:30 +0100

    Use suitable coverage tool for clang and gcc.

--- build.cmake
+++ build.cmake
@@ -4,9 +4,10 @@
 #
 # You can set some command line variables to change the
 # behaviour of this script:
-# CIO_CTEST_CONFIGURATION_TYPE:STRING=Debug|Release
-# CIO_CTEST_MODEL:STRING=Experimental|Nightly|Continuous
-# CIO_CTEST_COVERAGE:BOOL=OFF|ON
+# -DCIO_CTEST_CONFIGURATION_TYPE:STRING=Debug|Release
+# -DCIO_CTEST_MODEL:STRING=Experimental|Nightly|Continuous
+# -DCIO_CTEST_COVERAGE:BOOL=OFF|ON
+# -DCIO_CTEST_COMPILER:STRING=gcc|gcc-<version-number>|clang|clang-<version-number>
 
 set(CTEST_USE_LAUNCHERS 1)
 
@@ -21,11 +22,6 @@ endif()
 set(CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}")
 set(CTEST_BINARY_DIRECTORY "/tmp/cio/")
 
-cmake_host_system_information(RESULT FQDN QUERY FQDN)
-
-set(CTEST_BUILD_NAME "${FQDN}")
-set(CTEST_SITE "${FQDN}")
-
 if (NOT DEFINED CIO_CTEST_CONFIGURATION_TYPE)
     set(CIO_CTEST_CONFIGURATION_TYPE "Debug")
 endif()
@@ -37,6 +33,36 @@ if(NOT CIO_CTEST_MODEL STREQUAL "Experimental")
     ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 endif()
 
+if(NOT DEFINED CIO_CTEST_COMPILER)
+    set(CIO_CTEST_COMPILER "gcc")
+endif()
+
+string(REGEX MATCH "^gcc|^clang" C_COMPILER_TYPE ${CIO_CTEST_COMPILER})
+string(REPLACE ${C_COMPILER_TYPE} "" COMPILER_VERSION ${CIO_CTEST_COMPILER})
+if(C_COMPILER_TYPE STREQUAL "gcc")
+    set(CXX_COMPILER_TYPE "g++")
+    set(COVERAGE_TOOL "gcov${COMPILER_VERSION}")
+else()
+    set(CXX_COMPILER_TYPE "clang++")
+    set(COVERAGE_TOOL "llvm-cov${COMPILER_VERSION}")
+    set(CTEST_COVERAGE_EXTRA_FLAGS "gcov")
+endif()
+
+set(CONFIGURE_OPTIONS
+    "-DCMAKE_CXX_COMPILER=${CXX_COMPILER_TYPE}${COMPILER_VERSION}"
+    "-DCMAKE_C_COMPILER=${C_COMPILER_TYPE}${COMPILER_VERSION}"
+    "-DCMAKE_C_FLAGS_INIT=-pipe -fno-common"
+)
+
+if(CIO_CTEST_COVERAGE)
+    set(CONFIGURE_OPTIONS ${CONFIGURE_OPTIONS} "-DCMAKE_C_FLAGS=--coverage")
+endif()
+
+###########
+cmake_host_system_information(RESULT FQDN QUERY FQDN)
+
+set(CTEST_BUILD_NAME "${FQDN}")
+set(CTEST_SITE "${FQDN}")
 
 include(ProcessorCount)
 ProcessorCount(NUMBER_OF_CORES)
@@ -47,14 +73,7 @@ endif()
 ctest_start(${CIO_CTEST_MODEL})
 
 set(CTEST_CMAKE_GENERATOR "Ninja")
-set(COVERAGE_OPTIONS
-    "-DCMAKE_CXX_COMPILER=g++-8"
-    "-DCMAKE_C_COMPILER=gcc-8"
-    "-DCMAKE_C_FLAGS_INIT=pipe -fno-common"
-    "-DCMAKE_C_FLAGS=--coverage"
-)
-
-ctest_configure(OPTIONS "${COVERAGE_OPTIONS}")
+ctest_configure(OPTIONS "${CONFIGURE_OPTIONS}")
 
 
 # Test/Coverage step
@@ -69,9 +88,9 @@ if(CIO_CTEST_COVERAGE AND GCOVR_BIN)
     set(CIO_CTEST_COVERAGE_DIR "${CTEST_BINARY_DIRECTORY}cov-html/${CIO_CTEST_TIMESTAMP}")
     set(CTEST_CUSTOM_POST_TEST
         "cmake -E make_directory ${CIO_CTEST_COVERAGE_DIR}"
-        "${GCOVR_BIN} --gcov-executable=gcov-8 --html --html-details --html-title cio -f ${CTEST_SCRIPT_DIRECTORY}/src/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/http-parser/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/miniz/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/sha1/\\* --exclude-directories .\\*CompilerIdC\\* -r ${CTEST_SCRIPT_DIRECTORY} --object-directory=${CIO_OBJECT_DIRECTORY} -o ${CIO_CTEST_COVERAGE_DIR}/index.html")
+        "${GCOVR_BIN} \"--gcov-executable=${COVERAGE_TOOL} ${CTEST_COVERAGE_EXTRA_FLAGS}\" --html --html-details --html-title cio -f ${CTEST_SCRIPT_DIRECTORY}/src/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/http-parser/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/miniz/\\* -e ${CTEST_SCRIPT_DIRECTORY}/src/sha1/\\* --exclude-directories .\\*CompilerIdC\\* -r ${CTEST_SCRIPT_DIRECTORY} --object-directory=${CIO_OBJECT_DIRECTORY} -o ${CIO_CTEST_COVERAGE_DIR}/index.html")
     
-    set(CTEST_COVERAGE_COMMAND "gcov-8")
+    set(CTEST_COVERAGE_COMMAND "${COVERAGE_TOOL}")
     set(CTEST_CUSTOM_COVERAGE_EXCLUDE
     ${CTEST_CUSTOM_COVERAGE_EXCLUDE}
         ".*/tests/.*"
@@ -83,7 +102,12 @@ endif()
 if(CTEST_CMAKE_GENERATOR STREQUAL "Unix Makefiles")
     set(CTEST_BUILD_FLAGS -j${NUMBER_OF_CORES})
 endif()
-ctest_build()
+ctest_build(NUMBER_ERRORS CIO_NUMBER_OF_ERRORS NUMBER_WARNINGS CIO_NUMBER_OF_WARNING)
+if(CIO_NUMBER_OF_ERRORS OR CIO_NUMBER_OF_WARNING)
+    message(" -- Error or warning occured while building!")
+    return()
+endif()
+
 ctest_test(PARALLEL_LEVEL ${NUMBER_OF_CORES})
 
 if(CIO_CTEST_COVERAGE AND GCOVR_BIN)
@@ -95,7 +119,7 @@ find_program(CTEST_MEMORYCHECK_COMMAND NAMES valgrind)
 set(CTEST_MEMORYCHECK_TYPE Valgrind)
 set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--errors-for-leak-kinds=all --show-leak-kinds=all --leak-check=full --error-exitcode=1")
 #set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE ${CTEST_SOURCE_DIRECTORY}/tests/valgrind.supp)
-ctest_memcheck(PARALLEL_LEVEL ${NUMBER_OF_CORES})
+#ctest_memcheck(PARALLEL_LEVEL ${NUMBER_OF_CORES})
 
 # unset(CTEST_MEMORYCHECK_COMMAND)
 # unset(CTEST_MEMORYCHECK_COMMAND_OPTIONS)
