f19fe91fb0cf00e66026512ceb121e8f7b26a1a2
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-03-10 16:31:48 +0100

    Add toolchain file for Cortex M3

---
+++ cmake/gcc-arm-none-eabi.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_C_FLAGS_INIT "-mthumb -mcpu=cortex-m3 --specs=rdimon.specs")
+set(CMAKE_CXX_FLAGS_INIT "-mthumb -mcpu=cortex-m3 --specs=rdimon.specs")
