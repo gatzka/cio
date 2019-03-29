8496ddfabc0a13c94e1761018a28b3c46957f560
Stephan Gatzka <stephan.gatzka@hbm.com>
2019-03-29 15:39:32 +0100

    Rename toolchain files appropriately.

--- cmake/gcc-linux-arm.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
-
-set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
-set(CMAKE_C_FLAGS_INIT "-pipe -fno-common")
-set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm" "-L" "/usr/arm-linux-gnueabi/")
