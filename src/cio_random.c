4e703b8ad026ab78befd5c0bbac0cda80f66baea
Stephan Gatzka <stephan.gatzka@hbm.com>
2019-04-29 09:26:17 +0200

    Correct license.

--- src/cio_random.c
+++ src/cio_random.c
@@ -1,38 +1,35 @@
 /*
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: Apache-2.0
  *
- *The MIT License (MIT)
+ * PCG Random Number Generation for C.
  *
- * Copyright (c) <2019> <Stephan Gatzka>
+ * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information about the PCG random number generation scheme,
+ * including its license and other licensing options, visit
+ *
+ *     http://www.pcg-random.org
  */
 
 #include <stdint.h>
 
 #include "cio_random.h"
 
-struct pcg_state_setseq_64 {    // Internals are *Private*.
-	uint64_t state;             // RNG state.  All values are possible.
-	uint64_t inc;               // Controls which RNG sequence (stream) is selected. Must *always* be odd.
+struct pcg_state_setseq_64 {
+	uint64_t state; // RNG state. All values are possible.
+	uint64_t inc; // Controls which RNG sequence (stream) is selected. Must *always* be odd.
 };
 
 typedef struct pcg_state_setseq_64 pcg32_random_t;
