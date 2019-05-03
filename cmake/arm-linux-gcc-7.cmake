1c79ea9ac635f9ed90f1e01b84c3d81e9f686d71
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-05-03 09:48:01 +0200

    Correct arm linux toolchain file.

--- cmake/arm-linux-gcc-7.cmake
+++
@@ -1,8 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
-
-include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc8.cmake")
-
-set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc-7)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++-7)
-set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm" "-L" "/usr/arm-linux-gnueabi/")
