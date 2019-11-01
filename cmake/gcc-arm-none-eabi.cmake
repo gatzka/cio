c272adb94cabdfc140e234194335d68acb9bf8fe
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-11-01 09:56:22 +0100

    Remove toolchain files.
    The toolchain files are now fetched with cmake FetchContent.

--- cmake/gcc-arm-none-eabi.cmake
+++
@@ -1,8 +0,0 @@
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR ARM)
-
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
-set(CMAKE_C_FLAGS_INIT "-mthumb -mcpu=cortex-m3 --specs=rdimon.specs")
-set(CMAKE_CXX_FLAGS_INIT "-mthumb -mcpu=cortex-m3 --specs=rdimon.specs")
-set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm -cpu cortex-m3")
